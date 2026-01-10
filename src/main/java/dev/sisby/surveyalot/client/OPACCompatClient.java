5ea00f1b5c5b0cbff79f84c4e509fc0da443b5d4
Sisby folk <sleepingdragoninn@gmail.com>
2026-01-10 15:55:13 +1100

    Update OPACCompatClient.java

--- src/main/java/dev/sisby/surveyalot/client/OPACCompatClient.java
+++ src/main/java/dev/sisby/surveyalot/client/OPACCompatClient.java
@@ -2,13 +2,22 @@ package dev.sisby.surveyalot.client;
 
 import dev.sisby.surveyalot.OPACCompat;
 import folk.sisby.surveyor.client.SurveyorClientEvents;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.world.World;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.Identifier;
 import xaero.pac.client.event.api.OPACClientAddonRegister;
 
 public class OPACCompatClient {
+	public static RegistryKey<World> prevDim = null;
+	
 	public static void init() {
 		OPACClientAddonRegister.EVENT.register((c, r) -> c.register(new OPACCompat.SurveyalotListener(i -> MinecraftClient.getInstance().world != null && MinecraftClient.getInstance().world.getRegistryKey().getValue().equals(i) ? MinecraftClient.getInstance().world : null)));
-		SurveyorClientEvents.Register.worldLoad(Identifier.of("surveyalot", "opac"), (w, ws, p, t, s, l) -> OPACCompat.updateClaimLandmarksForDimension(w));
+		ClientTickEvents.END_WORLD_TICK.register((world -> { 
+			if (WorldSummary.of(world) != null && !world.getRegistryKey().equals(prevDim)) {
+				prevDim = world.getRegistryKey();
+				OPACCompat.updateClaimLandmarksForDimension(world);
+		    }
+	 	}
 	}
 }
